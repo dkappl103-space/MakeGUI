71a3e04184b686aafdd7090f80d845d236683f3f
dkappl103-space <dkappl103@gmail.com>
2025-10-18 12:19:07 -0700

    Add files via upload
    Fixed a couple things, had to meet assignment requirements. Made a rating button, be careful, whatever you type in it, will be printed to the console.

--- MakeGUI/MakeGUI/src/Main.java
+++ MakeGUI/MakeGUI/src/Main.java
@@ -2,31 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
-//Let me document my current plans for this, first I want to generate a lot of buttons that take up the screen
-//Then I need to make all but 1 button invisible, after you click it, it will be removed, and a new one will appear
-//This will be random hopefully, to make it different everytime you play the window.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Making a GUI");
-        //Makes a new Jframe called window to make a GUI to mess with
+        //Makes a new Jframe called window
         JFrame window = new JFrame("Making GUI");
-        //Sets the window size
+        //window size
         window.setSize(500, 500);
         //Makes it close when you close the window
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLayout(null);
-        //New panel called control
+        //New panel
         ImagePanel control = new ImagePanel("/CatLaughingMeme.jpg");
-
         control.setLayout(null);
         //Maximum place the panel (control) will take up
         control.setBounds(0, 0, 500, 500);
         window.add(control);
 
-        //Sets the panels preferred size
-        //control.setPreferredSize(new Dimension(400, 400));
+        //Funny meme stuff (Top Text)
+       JLabel titleLabel = new JLabel("Get a load of this guy");
+       titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+       titleLabel.setForeground(Color.WHITE);
+       titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+       titleLabel.setBounds(0, 10, 500, 40);
+       control.add(titleLabel);
 
-        //Wanted to spice things up a bit with something random
+        //Funny meme stuff (Botton Text)
+        JLabel newtitleLabel = new JLabel("Clicking all over the place");
+        newtitleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        newtitleLabel.setForeground(Color.WHITE);
+        newtitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        newtitleLabel.setBounds(0, 380, 500, 40);
+        control.add(newtitleLabel);
+
+
+
+        //For the buttons to be placed at random
         Random rando = new Random();
 
         //Add 30 buttons to press :)
@@ -54,6 +65,32 @@ public class Main {
             //Adds a button to the screen
             control.add(button);
         }
+        //Makes the text for the entry box
+        JLabel rate = new JLabel("Rate your experience: ");
+        rate.setFont(new  Font("Arial", Font.PLAIN, 16));
+        rate.setForeground(Color.WHITE);
+        rate.setBounds(0, 420, 200, 25);
+        control.add(rate);
+
+        //This allows you to type in a new box
+        JTextField rateInput = new JTextField();
+        rateInput.setBounds(150, 420, 180, 25);
+        control.add(rateInput);
+
+        //Submit the text you just entered for the rating
+        JButton submit = new JButton("Submit");
+        submit.setBounds(390,420, 80, 25);
+        control.add(submit);
+
+        //Prints what you type in the console
+        submit.addActionListener(e -> {
+           String rating = rateInput.getText();
+           System.out.println("Experience rated: " + rating);
+           window.dispose();
+        });
+
+
+
         //Makes a panel called control inside the window
         window.add(control);
         //Makes the window visible, so you can actually mess with it
